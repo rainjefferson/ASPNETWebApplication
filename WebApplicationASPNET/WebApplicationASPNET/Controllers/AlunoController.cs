c6a08a9129d193b61e033127e1ea05939f4e735e
rainjefferson <117676799+rainjefferson@users.noreply.github.com>
2023-09-30 10:55:55 -0300

    Commit apos incluir Controller AlunoController.cs

---
+++ WebApplicationASPNET/WebApplicationASPNET/Controllers/AlunoController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplicationASPNET.Controllers
+{
+    public class AlunoController : ApiController
+    {
+        // GET: api/Aluno
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Jefferson", "Rain" };
+        }
+
+        // GET: api/Aluno/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Aluno
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Aluno/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Aluno/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
